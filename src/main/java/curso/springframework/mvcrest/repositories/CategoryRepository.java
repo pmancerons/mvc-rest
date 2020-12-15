46c1f5477be7ce31e538d1fe8ed411be30d93862
Pedro <pmancerons@gmail.com>
2020-12-15 16:04:54 -0500

    initial version of project

---
+++ src/main/java/curso/springframework/mvcrest/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package curso.springframework.mvcrest.repositories;
+
+import curso.springframework.mvcrest.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository  extends JpaRepository<Category, Long> {
+}
---
+++ src/test/java/curso/springframework/mvcrest/MvcRestApplicationTests.java
@@ -0,0 +1,13 @@
+package curso.springframework.mvcrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
